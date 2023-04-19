3cf9e7d8c9d1c21d3320115e3cddea23f1b740aa
Santiago Paiz <paizsantiago5@gmail.com>
2023-04-19 14:15:39 -0300

    solucionando bug

--- src/Services/carritoService.js
+++ src/Services/carritoService.js
@@ -1,8 +1,8 @@
-const {DAO} = require('../daos/factory');
+const { DAO } = require('../daos/factory');
 const Usuarios = require('../../models/usuario');
 const { sendMsg, sendWspp } = require('../../Config/twilio');
 const { mailCompraFinalizada } = require('../../Config/nodemailer');
-const { loggerError } = require('../../Config/loggerConfig')
+const { loggerError } = require('../../Config/loggerConfig');
 
 const getCarrito = async (req) => {
   try {
@@ -15,33 +15,35 @@ const getCarrito = async (req) => {
     const carritoFinal = { productsCart, productsExist };
     return carritoFinal;
   } catch (error) {
-    loggerError.error({msg: `${error}`})
+    loggerError.error({ msg: `${error}` });
   }
 };
 
-const postProductCart = async (req) => { // agrega un producto al cart
+const postProductCart = async (req) => {
+  // agrega un producto al cart
   try {
     const { id } = req.params;
-  const username = req.user.email;
-  const user = await Usuarios.find({ email: username });
-  const carritoUserID = user[0].carrito._id;
-  const carritoUserTimestamp = user[0].carrito.timestamp;
-  const carritoDB = await DAO.carrito.getById(carritoUserID);
-  let newProductos = carritoDB.productos;
-  const productoPedido = await DAO.productos.getById(id);
-  newProductos = [...newProductos, productoPedido];
-  await DAO.carrito.updateCartById(
-    carritoUserID,
-    carritoUserTimestamp,
-    newProductos
-  );
-  return true;
+    const username = req.user.email;
+    const user = await Usuarios.find({ email: username });
+    const carritoUserID = user[0].carrito._id;
+    const carritoUserTimestamp = user[0].carrito.timestamp;
+    const carritoDB = await DAO.carrito.getById(carritoUserID);
+    let newProductos = carritoDB.productos;
+    const productoPedido = await DAO.productos.getById(id);
+    newProductos = [...newProductos, productoPedido];
+    await DAO.carrito.updateCartById(
+      carritoUserID,
+      carritoUserTimestamp,
+      newProductos
+    );
+    return true;
   } catch (error) {
-    loggerError.error({msg: `${error}`})
+    loggerError.error({ msg: `${error}` });
   }
 };
 
-const postActualizarCarrito = async (req) => { // elimina un producto del cart
+const postActualizarCarrito = async (req) => {
+  // elimina un producto del cart
   try {
     const { id } = req.params;
     const username = req.user.email;
@@ -53,7 +55,7 @@ const postActualizarCarrito = async (req) => { // elimina un producto del cart
     let newProductos = carritoDB.productos;
     newProductos.length === 1
       ? (carritoActualizado = [])
-      : (carritoActualizado = newProductos.find((item) => item._id !== id));
+      : (carritoActualizado = newProductos.filter((item) => item._id != id));
     await DAO.carrito.updateCartById(
       carritoUserID,
       carritoUserTimestamp,
@@ -61,11 +63,12 @@ const postActualizarCarrito = async (req) => { // elimina un producto del cart
     );
     return true;
   } catch (error) {
-    loggerError.error({msg: `${error}`})
+    loggerError.error({ msg: `${error}` });
   }
 };
 
-const postFinalizarCompra = async (req) => { // envia el mail y msg una vez finalizada la compra
+const postFinalizarCompra = async (req) => {
+  // envia el mail y msg una vez finalizada la compra
   try {
     const vaciarCarrito = [];
     const username = req.user.email;
@@ -82,7 +85,7 @@ const postFinalizarCompra = async (req) => { // envia el mail y msg una vez fina
     sendMsg(user[0].telefono);
     sendWspp(user[0], carritoDB.productos);
   } catch (error) {
-    loggerError.error({msg: `${error}`})
+    loggerError.error({ msg: `${error}` });
   }
 };
 
